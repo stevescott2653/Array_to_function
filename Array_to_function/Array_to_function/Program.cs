843804b842c0578f5ba8d357b4fdefe9838af8b3
Steven Scott <stevescott2653@gmail.com>
2019-09-26 13:08:57 -0400

    Add files via upload

---
+++ Array_to_function/Array_to_function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_to_function
+{
+    class Program
+    {
+        static void printArray(int[] arr)
+        {
+            Console.WriteLine("Printing array elements: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 25, 10, 20, 15, 40, 50 };
+            int[] arr2 = { 12, 23, 44, 11, 54 };
+            printArray(arr1);
+            printArray(arr2);
+        }
+    }
+}
